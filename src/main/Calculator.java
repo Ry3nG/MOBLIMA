67f41f61f9cc061112b071e9419ddc375f2c9458
ZERUI GONG <Nomlasssssss@outlook.com>
2022-10-11 13:35:57 +0800

    initial commit

---
+++ src/main/Calculator.java
@@ -0,0 +1,16 @@
+package main;
+/**
+ * A simple Calculator class. 
+ * Helps to illustrate how unit test works.
+ */
+public class Calculator {
+    /**
+     * Adds two numbers.
+     * @param a the first number
+     * @param b the second number
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
