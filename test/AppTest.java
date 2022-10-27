4f78484b7540ecc7f042e161e5528153694f3430
Crystal Cheong <65748007+crystalcheong@users.noreply.github.com>
2022-10-28 02:03:22 +0800

    Omitted runner

---
+++ test/AppTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("AppTest")
+public class AppTest {
+  private static App app;
+  private static final int MAX_TIMEOUT = 5;
+
+  @BeforeAll
+  public static void testInit() {
+    app = App.getInstance();
+    assertNotNull(app, "App instance was null");
+  }
+
+  @Test
+  public void testDefault() {
+    String[] args = {};
+    Runnable runApp = () -> assertDoesNotThrow(() -> this.app.main(args));
+
+    Future<String> executor
+        = (Future<String>) Executors.newSingleThreadExecutor().submit(runApp);
+    try {
+      executor.get(MAX_TIMEOUT, TimeUnit.SECONDS);
+    } catch (TimeoutException ex) {
+      executor.cancel(true);
+    } catch (Exception ex) {
+      fail("[FAIL/AppTest.testDefault] Executor did not timeout as expected");
+    }
+  }
+
+  @Test
+  public void testDebug() {
+    String[] args = {"--debug"};
+    Runnable runApp = () -> assertDoesNotThrow(() -> this.app.main(args));
+
+    Future<String> executor
+        = (Future<String>) Executors.newSingleThreadExecutor().submit(runApp);
+    try {
+      executor.get(MAX_TIMEOUT, TimeUnit.SECONDS);
+    } catch (TimeoutException ex) {
+      executor.cancel(true);
+    } catch (Exception ex) {
+      fail("[FAIL/AppTest.testDefault] Executor did not timeout as expected");
+    }
+  }
+
+  @Test
+  public void testStaff() {
+    String[] args = {"--staff"};
+    Runnable runApp = () -> assertDoesNotThrow(() -> this.app.main(args));
+
+    Future<String> executor
+        = (Future<String>) Executors.newSingleThreadExecutor().submit(runApp);
+    try {
+      executor.get(MAX_TIMEOUT, TimeUnit.SECONDS);
+    } catch (TimeoutException ex) {
+      executor.cancel(true);
+    } catch (Exception ex) {
+      fail("[FAIL/AppTest.testDefault] Executor did not timeout as expected");
+    }
+  }
+}
